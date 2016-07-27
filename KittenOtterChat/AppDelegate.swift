3475cecb24a9fa7cceb65ed2506278fee1413f50
Freddie Meyer <dungfu@gmail.com>
2016-07-27 14:05:04 -0700

    first commit of initial code

---
+++ KittenOtterChat/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KittenOtterChat
+//
+//  Created by Freddie Meyer on 7/27/16.
+//  Copyright © 2016 Freddie Meyer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
